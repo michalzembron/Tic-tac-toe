1bdd97d9c378332c0b66959495857877b6ff0e07
Michał Zembroń <michalzembron@gmail.com>
2021-03-28 16:55:03 +0200

    Play_Singleplayer small changes
    + changed ImageButtons to display default button background
    + changed player numbering to more logical (p1 = 1, p2 = 2, none = 0)
    + tiny refactor

--- app/src/main/java/com/example/tictactoe/Play_Singleplayer.java
+++ app/src/main/java/com/example/tictactoe/Play_Singleplayer.java
@@ -2,7 +2,7 @@ package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,18 +14,18 @@ import android.widget.Toast;
 public class Play_Singleplayer extends AppCompatActivity implements View.OnClickListener{
 
     private TextView playerOneScore, playerTwoScore, playerStatus;
-    private ImageButton [] buttons = new ImageButton[9];
+    private final ImageButton [] buttons = new ImageButton[9];
     private Button resetGame;
 
     private int playerOneScoreCount, playerTwoScoreCount, roundCount;
     boolean activePlayer;
 
-    // p1 => 0
-    // p2 => 1
-    // empty => 2
-    int [] gameState = {2,2,2,
-                        2,2,2,
-                        2,2,2};
+    // empty => 0
+    // p1 => 1
+    // p2 => 2
+    int [] gameState = {0,0,0,
+                        0,0,0,
+                        0,0,0};
 
     int [][] winningPositions = {
             {0,1,2}, {3,4,5}, {6,7,8}, //wiersze
@@ -33,46 +33,31 @@ public class Play_Singleplayer extends AppCompatActivity implements View.OnClick
             {0,4,8}, {2,4,6} //na krzyz
     };
 
-    //private short currentPlayer=0;
-    //private short boardSize = 3;
-    //private String[][] board = new String[boardSize][boardSize];
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play__singleplayer);
-
-        /*
-        for (int i = 0; i < boardSize; i++){
-            for (int j = 0; j < boardSize; j++){
-                board[i][j] = "-";
-            }
-        }
-        */
-
-        //Log.d("DEBUG", "board: " + Arrays.deepToString(board));
         AssignVariables();
     }
 
     @Override
     public void onClick(View v) {
-        Log.i("test", "button nacisniety!");
-        if (v.getTag()!="2")
+        if (v.getTag()!="0")
         {
             return;
         }
         String buttonID = v.getResources().getResourceEntryName(v.getId());
-        int gameStatePointer = Integer.parseInt(buttonID.substring(buttonID.length()-1, buttonID.length()));
+        int gameStatePointer = Integer.parseInt(buttonID.substring(buttonID.length()-1));
 
         if (activePlayer){
             //TODO: zrobic rozne ikonki albo standaryzacje
             ((ImageButton) v).setImageResource(R.drawable.ic_person_black_24dp);
-            v.setTag("0");
-            gameState[gameStatePointer] = 0;
-        } else {
-            ((ImageButton) v).setImageResource(R.drawable.ic_people_alt_black_24dp);
             v.setTag("1");
             gameState[gameStatePointer] = 1;
+        } else {
+            ((ImageButton) v).setImageResource(R.drawable.ic_people_alt_black_24dp);
+            v.setTag("2");
+            gameState[gameStatePointer] = 2;
         }
         roundCount++;
 
@@ -81,13 +66,12 @@ public class Play_Singleplayer extends AppCompatActivity implements View.OnClick
                 playerOneScoreCount++;
                 UpdatePlayerScore();
                 Toast.makeText(this, "Player One Won!", Toast.LENGTH_SHORT).show();
-                PlayAgain();
             } else {
                 playerTwoScoreCount++;
                 UpdatePlayerScore();
                 Toast.makeText(this, "Player Two Won!", Toast.LENGTH_SHORT).show();
-                PlayAgain();
             }
+            PlayAgain();
         } else if (roundCount == 9) {
             PlayAgain();
             Toast.makeText(this, "No Winner!", Toast.LENGTH_SHORT).show();
@@ -96,22 +80,19 @@ public class Play_Singleplayer extends AppCompatActivity implements View.OnClick
         }
 
         if (playerOneScoreCount > playerTwoScoreCount){
-            playerStatus.setText("Player One is Winning !");
+            playerStatus.setText(R.string.p1Winning);
         } else if (playerOneScoreCount < playerTwoScoreCount) {
-            playerStatus.setText("Player Two is Winning !");
+            playerStatus.setText(R.string.p2Winning);
         } else {
             playerStatus.setText("");
         }
 
-        resetGame.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                PlayAgain();
-                playerOneScoreCount = 0;
-                playerTwoScoreCount = 0;
-                playerStatus.setText("");
-                UpdatePlayerScore();
-            }
+        resetGame.setOnClickListener(v1 -> {
+            PlayAgain();
+            playerOneScoreCount = 0;
+            playerTwoScoreCount = 0;
+            playerStatus.setText("");
+            UpdatePlayerScore();
         });
     }
 
@@ -119,16 +100,18 @@ public class Play_Singleplayer extends AppCompatActivity implements View.OnClick
         boolean winnerResult = false;
 
         for(int [] winningPosition : winningPositions){
-            if(gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
+            if (gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
                     gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
-                    gameState[winningPosition[0]] != 2){
+                    gameState[winningPosition[0]] != 0) {
                 winnerResult = true;
+                break;
             }
         }
 
         return winnerResult;
     }
 
+    @SuppressLint("SetTextI18n")
     public void UpdatePlayerScore(){
         playerOneScore.setText(Integer.toString(playerOneScoreCount));
         playerTwoScore.setText(Integer.toString(playerTwoScoreCount));
@@ -138,9 +121,9 @@ public class Play_Singleplayer extends AppCompatActivity implements View.OnClick
         roundCount = 0;
         activePlayer = true;
         for(int i = 0; i < buttons.length; i++){
-            gameState[i] = 2;
-            buttons[i].setImageResource(R.drawable.ic_launcher_background);
-            buttons[i].setTag("2");
+            gameState[i] = 0;
+            buttons[i].setImageResource(0);
+            buttons[i].setTag("0");
         }
     }
 
@@ -156,7 +139,7 @@ public class Play_Singleplayer extends AppCompatActivity implements View.OnClick
             int resourceID = getResources().getIdentifier(buttonID, "id", getPackageName());
             buttons[i] = (ImageButton) findViewById(resourceID);
             buttons[i].setOnClickListener(this);
-            buttons[i].setTag("2");
+            buttons[i].setTag("0");
         }
 
         roundCount = 0;
